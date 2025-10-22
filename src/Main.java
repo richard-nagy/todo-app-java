3227a8f5240445e5fba2f39e4db77dc10bb2fc7d
richard-nagy <big.ricsoo@gmail.com>
2025-10-22 22:36:21 +0200

    Move logic to different methods

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,54 @@
+static public int getIntFromUser(Scanner scanner) {
+    String input = scanner.nextLine();
+
+    try {
+        return Integer.parseInt(input);
+    } catch (NumberFormatException e) {
+        IO.println("Invalid input, try again.");
+        return -1;
+    }
+}
+
+static public int displayMenu(Scanner scanner) {
+    IO.println("\nMenu:");
+    IO.println("[0] Exit");
+    IO.println("[1] Add task");
+    IO.println("[2] View tasks");
+
+    IO.print("Enter choice: ");
+    return getIntFromUser(scanner);
+}
+
+static public void addTask(ArrayList<String> tasks, Scanner scanner) {
+    IO.print("Enter task: ");
+    tasks.add(scanner.nextLine());
+}
+
+static public void getTasks(ArrayList<String> tasks) {
+    IO.println("Tasks:");
+    for (String task : tasks) IO.println("- " + task);
+}
+
 void main() {
     Scanner scanner = new Scanner(System.in);
     ArrayList<String> tasks = new ArrayList<>();
     boolean exit = false;
 
     while (!exit) {
-        IO.println("\nMenu:");
-        IO.println("[0] Exit");
-        IO.println("[1] Add task");
-        IO.println("[2] View tasks");
+        int choice = displayMenu(scanner);
+        if (choice == 0) exit = true;
 
-        IO.print("Enter choice: ");
-        String input = scanner.nextLine();
-        int choice;
-
-        try {
-            choice = Integer.parseInt(input);
-        } catch (NumberFormatException e) {
-            IO.println("Invalid input, try again.");
+        else if (choice == -1) {
+            IO.println("Unknown option, try again.");
             continue;
         }
 
-        if (choice == 0) exit = true;
-
         switch (choice) {
             case 1:
-                IO.print("Enter task: ");
-                tasks.add(scanner.nextLine());
+                addTask(tasks, scanner);
                 break;
             case 2:
-                IO.println("Tasks:");
-                for (String task : tasks) IO.println("- " + task);
+                getTasks(tasks);
                 break;
             default:
                 IO.println("Unknown option, try again.");
